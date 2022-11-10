eb24dd0ca4085bae997a617b928999b23d537cf8
Mohamed El-Haddad <112962278+Haddad308@users.noreply.github.com>
2022-11-10 17:07:37 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ import joblib
 model = joblib.load("model.h5")
 scalar = joblib.load("scalar.h5")
 
-st.markdown(f"{model.predict(scalar.transform([[19,0,24,0,1,0,1,1,0]]))[0]}")
+st.markdown(f"ركز يا عبد الكرييييم ")
